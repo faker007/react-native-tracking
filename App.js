11fcd6cc8ed922a03077e54d2a2bd4e659db4852
faker007 <rlaqjadud195@naver.com>
2020-10-12 13:50:49 +0900

    add navigation setup

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
-import AccountScreen from "./src/screens/AccountScreen";
+import React from "react";
+
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import AccountScreen from "./src/s creens/AccountScreen";
 import SigninScreen from "./src/screens/SigninScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import TrackCreateScreen from "./src/screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/screens/TrackDetailScreen";
 import TrackListScreen from "./src/screens/TrackListScreen";
+
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    SignupScreen: SignupScreen,
+    SigninScreen: SigninScreen,
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen,
+    }),
+    TreackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  }),
+});
